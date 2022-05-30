991e7e04bd1d00beb19d613d8d31f6b274811357
stefan.sujdovic <stefan.sujdovic@authentickated.com>
2022-05-30 22:45:02 +0200

    added mobile nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,81 @@
 import "../styles/Header.css";
 
+import React, { useState } from "react";
+
 const Header = () => {
+  const [openNavigation, setOpenNavigation] = useState(false);
+
+  const toogleNav = () => {
+    setOpenNavigation(!openNavigation);
+  };
+
   return (
     <>
       <header>
         <section className="mobile-nav">
-          <img src="/images/mobile_nav/logo.svg" alt="Logo" title="Logo" />
+          {!openNavigation && (
+            <img src="/images/mobile_nav/logo.svg" alt="Logo" title="Logo" />
+          )}
+
+          {openNavigation && (
+            <img
+              className="mobile-logo-active"
+              src="/images/mobile_nav/logo_active.svg"
+              alt="Logo"
+              title="Logo"
+            />
+          )}
 
           <img
-            src="/images/mobile_nav/navigation.svg"
+            onClick={toogleNav}
+            className="mobile-logo-active"
+            src={
+              `/images/mobile_nav/${openNavigation ? "close" : "navigation"}` +
+              ".svg"
+            }
             alt="Navigation"
             title="Navigation"
           />
-        </section>
-        <section className="desktop-nav">
-          <img src="/images/desktop_nav/logo.svg" alt="Logo" title="Logo" />
-          <nav>
-              <ul className="desktop-item-container">
+
+          {openNavigation && (
+            <div className="mobile-item-container">
+              <nav>
+                <ul>
                   <li>Home</li>
                   <li className="active">Earn</li>
                   <li>Promote</li>
                   <li>Join Waitlist</li>
-              </ul>
+                </ul>
+              </nav>
+              <div className="social_header">
+                <img
+                  src="/images/footer/linkedin.svg"
+                  alt="LinkedIn"
+                  title="LinkedIn"
+                />
+                <img
+                  src="/images/footer/telegram.svg"
+                  alt="Telegram"
+                  title="Telegram"
+                />
+                <img
+                  src="/images/footer/twitter.svg"
+                  alt="Twitter"
+                  title="Twitter"
+                />
+              </div>
+            </div>
+          )}
+        </section>
+        <section className="desktop-nav">
+          <img src="/images/desktop_nav/logo.svg" alt="Logo" title="Logo" />
+          <nav>
+            <ul className="desktop-item-container">
+              <li>Home</li>
+              <li className="active">Earn</li>
+              <li>Promote</li>
+              <li>Join Waitlist</li>
+            </ul>
           </nav>
         </section>
       </header>
