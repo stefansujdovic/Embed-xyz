1e557eec999199ddf86a81eb0d429d4af878f953
stefan.sujdovic <stefan.sujdovic@authentickated.com>
2022-05-30 19:53:40 +0200

    added Earn Model

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Layout from "../src/components/Layout"
 import HomeVideo from "./components/HomeVideo"
 import LeadTheChange from "./components/LeadTheChange";
+import EarnModel from "./components/EarnModel";
 
 function App() {
   return (
     <Layout>
       <HomeVideo />
       <LeadTheChange />
+      <EarnModel />
     </Layout>
   );
 }
---
+++ src/components/EarnModel.js
@@ -0,0 +1,26 @@
+import "../styles/EarnModel.css";
+
+const EarnModel = () => {
+    return (
+        <section className="earn-model-container">
+            <h3 className="section-title text-center">The Embed-to-Earn Model</h3>
+
+            <img
+                className="earn-model-destkop m-auto"
+                src="/images/earn_model/earn_model_desktop.svg"
+                alt="Earn Model"
+                title="Earn Model"
+            />
+
+            <img
+                className="earn-model-mobile"
+                src="/images/earn_model/earn_model_mobile.svg"
+                alt="Earn Model"
+                width={"100%"}
+                title="Earn Model"
+            />
+        </section>
+    );
+};
+
+export default EarnModel;
