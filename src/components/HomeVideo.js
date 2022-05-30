39e49dbb7e18755247c22ec9bd94657a880c48ae
stefan.sujdovic <stefan.sujdovic@authentickated.com>
2022-05-30 18:33:42 +0200

    added fonts and responsive homevideojs

--- src/components/HomeVideo.js
+++ src/components/HomeVideo.js
@@ -9,15 +9,29 @@ const HomeVideo = () => {
       <div className="home-video-overlay">
         <div>
           <img
+            className="white-logo mb2"
             src="/images/home_video/logo.svg"
             alt="Logo white"
             title="Logo White"
           />
         </div>
-        <div>
-          <h1 className="home-title">
+        <div className="video-inner-text">
+          <h1 className="home-main-title m0">
             Embed empowers you to monetize your portfolio
           </h1>
+          <hr className="home_border" />
+          <p className="home-subtitle">
+            We enable NTF holders to become influencers, build their audience
+            through a turn-key solution developed to monetize and transform
+            their portfolio into a yielding asset.We create passion-based
+            communities.
+          </p>
+          <div className="module-border-wrap cp">
+            <div className="module d-flex flex-row jcbtw">
+              Join waitlist
+              <img src="images/home_video/right_arrow.svg" alt="Right arrow" title="Right arrow" />
+            </div>
+          </div>
         </div>
       </div>
     </section>
